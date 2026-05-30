caf07094cfc59ed76dfd13a0e4e253f75a75e9ad
microwavetaco67 <arthurmascher9@gmail.com>
2026-05-30 13:40:55 +0200

    aaaa

--- src/main/java/micro/crack2/CrackmodII.java
+++ src/main/java/micro/crack2/CrackmodII.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//aaaa
 public class CrackmodII implements ModInitializer {
 	public static final String MOD_ID = "crackmod2";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
